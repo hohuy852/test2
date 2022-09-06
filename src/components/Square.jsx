edeeda38a430a31ef317ee177fbd8c7173ce3e43
tuannm <j.org.nmt@gmail.com>
2022-09-06 22:27:42 +0700

    first commit

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Square = () => {
   return (
-    <div className='h-52 w-52 bg-white mx-auto my-10'></div>
+    <div className='h-52 w-52 bg-white mx-auto my-14'></div>
   )
 }
 
